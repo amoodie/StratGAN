770b8de229408b27d66b65a754e5638be7ae2938
amoodie <amoodie@rice.edu>
2018-09-19 23:39:25 -0500

    working, except for parser

--- StratGAN/loader.py
+++ StratGAN/loader.py
@@ -13,54 +13,75 @@ class BaseImageProvider(object):
     This class provides a basis for generating a tf.data.Dataset and Iterator.
     It takes some hints the UNet implementation in tf.
     """
-    def __init__(self, image_dir, c_dim=None, a_min=None, a_max=None):
-        self.a_min = a_min if a_min is not None else -np.inf
-        self.a_max = a_max if a_min is not None else np.inf
+    def __init__(self, image_dir, image_ext='*.png', 
+                 c_dim=None, a_min=None, a_max=None, verbose=False):
+
+        self.verbose = verbose
 
         self.image_dir = image_dir
-        self._image_list = self._list_images(self.image_dir)
+        self.image_ext = image_ext
+        self._image_list = self._list_images(os.path.join(self.image_dir, self.image_ext))
         self._label_list = self._parse_labels(self._image_list)
 
         assert len(self._image_list) > 0, "No training files"
         assert len(self._image_list) == len(self._label_list), "Unequal images/labels length"
-        print("Number of files used: %s" % len(self._image_list))
 
-        if channels is None:
+        self.a_min = a_min if a_min is not None else -np.inf
+        self.a_max = a_max if a_min is not None else np.inf
+
+        # try:
+        test_img = self.__load_image(self._image_list[0])
+        # except:
+            # Exception('could not load a test image, aborting...')
+
+        if c_dim is None:
             print('Number of channels (c_dim) not provided, attempting to determine...')
-            test_img = __load_image(self, os.path.join(self.image_dir, self._image_list[0]))
-            self.c_dim = test_img.shape[2]
+            self.c_dim = test_img.shape[-1]
             print('Tested image had {0} dimension(s)'.format(self.c_dim))
         else:
             self.c_dim = c_dim
 
-        self.x_dim = 
-        self.y_dim = 
+        self.x_dim = test_img.shape[0]
+        self.y_dim = test_img.shape[1]
+
+        if self.verbose:
+            self.print_data_info()
 
 
     def _list_images(self, image_dir):
-        all_files = glob.glob(search_path)
+        print('imagedir:', image_dir)
+        all_files = glob.glob(image_dir)
         return [name for name in all_files]
 
-    def _parse_labels(self, list):
-        return split
+    def _parse_labels(self, image_list):
+        # this is not robust to more that 10 classes...
+        labels = [path[0] for path in image_list]
+        print(labels)
+        return 
 
     def __load_image(self, path, dtype=np.float32):
         """
         single image reader, used for testing image to determine values if not given
         """
+        print(path)
         try:
             img_array = np.array(Image.open(path), dtype)
         except:
             img_array = np.squeeze(cv2.imread(image_name, cv2.IMREAD_GRAYSCALE))
         return img_array
 
+    def print_data_info(self):
+        print("Number of images: %s" % len(self._image_list))
+        print("Categories in labels: %s" % np.unique(np.array([l for l in self._label_list]))).size
+
 
 
 
 class ImageDatasetProvider(BaseImageProvider):
-    def __init__(self, image_dir, c_dim=None, batch_size=None, 
-                 shuffle_data=True, buffer_size=1, repeat_data=True, a_min=None, a_max=None):
-        super(BaseImageProvider, self).__init__(image_dir, a_min, a_max)
+    def __init__(self, image_dir, image_ext='*.png', c_dim=None, batch_size=None, 
+                 shuffle_data=True, buffer_size=1, repeat_data=True,
+                 a_min=None, a_max=None, verbose=False):
+        super().__init__(image_dir, image_ext, c_dim, a_min, a_max, verbose)
         
         self.shuffle_data = shuffle_data
         self.repeat_data = repeat_data
@@ -68,15 +89,18 @@ class ImageDatasetProvider(BaseImageProvider):
 
         # convert to constants for tf
         self.filenames = tf.constant(self._image_list)
-        self.labels = tf.constant(_label_list)
+        self.labels = tf.constant(self._label_list)
 
         # create dataset
         self.data = tf.data.Dataset.from_tensor_slices((self.filenames, self.labels))
 
         # map image in the dataset
-        self.data = self.data.map(self._load_image_func)
+        self.data = self.data.map(lambda s: s._load_image_func(self.c_dim))
 
         # process options to the dataset object
+        if len(self.data) < self.batch_size:
+            raise Exception("dataset size is less than batch_size")
+
         if batch_size is not None:
             self.batch_size = batch_size
         else:
@@ -95,12 +119,12 @@ class ImageDatasetProvider(BaseImageProvider):
         self.image_batch, self.label_batch = iterator.get_next()
         
 
-    def _load_image_func(filename, label):
+    def _load_image_func(filename, label, c_dim):
         """
         load image function used to batch the files
         """
         image_string = tf.read_file(filename)
-        image_decoded = tf.image.decode_jpeg(image_string, c_dim=3)
+        image_decoded = tf.image.decode_jpeg(image_string, c_dim=c_dim)
         image = tf.cast(image_decoded, tf.float32)
         return image, label    
 
