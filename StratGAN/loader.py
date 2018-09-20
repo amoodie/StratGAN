8b297421520d1ad4c13fd6cf96abe8a766ec095d
amoodie <amoodie@rice.edu>
2018-09-19 22:44:12 -0500

    introduce dataset for image serving

--- StratGAN/loader.py
+++ StratGAN/loader.py
@@ -8,190 +8,99 @@ from PIL import Image
 import os
 import utils
 
-class BASE(object):
-    ds_train = tf.data.Dataset.from_tensor_slices((X_train, y_train)).repeat().batch(batch_size).shuffle(buffer_size)
-    it_train = ds_train.make_one_shot_iterator()
-    xs, ys = it_train.get_next()
-
-
-
-
-
-class BaseDataProvider(object):
+class BaseImageProvider(object):
     """
-    Abstract base class for DataProvider implementation. Subclasses have to
-    overwrite the `_next_data` method that load the next data and label array.
-    This implementation automatically clips the data with the given min/max and
-    normalizes the values to (0,1]. To change this behavoir the `_process_data`
-    method can be overwritten. To enable some post processing such as data
-    augmentation the `_post_process` method can be overwritten.
-
-    :param a_min: (optional) min value used for clipping
-    :param a_max: (optional) max value used for clipping
-
+    This class provides a basis for generating a tf.data.Dataset and Iterator.
+    It takes some hints the UNet implementation in tf.
     """
-    
-    channels = 1
-    n_class = 2
-    
-
-    def __init__(self, a_min=None, a_max=None):
+    def __init__(self, image_dir, c_dim=None, a_min=None, a_max=None):
         self.a_min = a_min if a_min is not None else -np.inf
         self.a_max = a_max if a_min is not None else np.inf
 
-    def _load_data_and_label(self):
-        data, label = self._next_data()
-            
-        train_data = self._process_data(data)
-        labels = self._process_labels(label)
-        
-        train_data, labels = self._post_process(train_data, labels)
-        
-        nx = train_data.shape[1]
-        ny = train_data.shape[0]
+        self.image_dir = image_dir
+        self._image_list = self._list_images(self.image_dir)
+        self._label_list = self._parse_labels(self._image_list)
 
-        return train_data.reshape(1, ny, nx, self.channels), labels.reshape(1, ny, nx, self.n_class),
-    
-    def _process_labels(self, label):
-        if self.n_class == 2:
-            nx = label.shape[1]
-            ny = label.shape[0]
-            labels = np.zeros((ny, nx, self.n_class), dtype=np.float32)
-            labels[..., 1] = label
-            labels[..., 0] = ~label
-            return labels
-        
-        return label
-    
-    def _process_data(self, data):
-        # normalization
-        data = np.clip(np.fabs(data), self.a_min, self.a_max)
-        data -= np.amin(data)
-        data /= np.amax(data)
-        return data
-    
-    def _post_process(self, data, labels):
+        assert len(self._image_list) > 0, "No training files"
+        assert len(self._image_list) == len(self._label_list), "Unequal images/labels length"
+        print("Number of files used: %s" % len(self._image_list))
+
+        if channels is None:
+            print('Number of channels (c_dim) not provided, attempting to determine...')
+            test_img = __load_image(self, os.path.join(self.image_dir, self._image_list[0]))
+            self.c_dim = test_img.shape[2]
+            print('Tested image had {0} dimension(s)'.format(self.c_dim))
+        else:
+            self.c_dim = c_dim
+
+        self.x_dim = 
+        self.y_dim = 
+
+
+    def _list_images(self, image_dir):
+        all_files = glob.glob(search_path)
+        return [name for name in all_files]
+
+    def _parse_labels(self, list):
+        return split
+
+    def __load_image(self, path, dtype=np.float32):
         """
-        Post processing hook that can be used for data augmentation
-        
-        :param data: the data array
-        :param labels: the label array
+        single image reader, used for testing image to determine values if not given
         """
-        return data, labels
-    
-    def __call__(self, n):
-        train_data, labels = self._load_data_and_label()
-        nx = train_data.shape[1]
-        ny = train_data.shape[2]
-    
-        X = np.zeros((n, nx, ny, self.channels))
-        Y = np.zeros((n, nx, ny, self.n_class))
-    
-        X[0] = train_data
-        Y[0] = labels
-        for i in range(1, n):
-            train_data, labels = self._load_data_and_label()
-            X[i] = train_data
-            Y[i] = labels
-    
-        return X, Y
-    
-class SimpleDataProvider(BaseDataProvider):
-    """
-    A simple data provider for numpy arrays. 
-    Assumes that the data and label are numpy array with the dimensions
-    data `[n, X, Y, channels]`, label `[n, X, Y, classes]`. Where
-    `n` is the number of images, `X`, `Y` the size of the image.
-
-    :param data: data numpy array. Shape=[n, X, Y, channels]
-    :param label: label numpy array. Shape=[n, X, Y, classes]
-    :param a_min: (optional) min value used for clipping
-    :param a_max: (optional) max value used for clipping
-    :param channels: (optional) number of channels, default=1
-    :param n_class: (optional) number of classes, default=2
-    
-    """
-    
-    def __init__(self, data, label, a_min=None, a_max=None, channels=1, n_class = 2):
-        super(SimpleDataProvider, self).__init__(a_min, a_max)
-        self.data = data
-        self.label = label
-        self.file_count = data.shape[0]
-        self.n_class = n_class
-        self.channels = channels
-
-    def _next_data(self):
-        idx = np.random.choice(self.file_count)
-        return self.data[idx], self.label[idx]
+        try:
+            img_array = np.array(Image.open(path), dtype)
+        except:
+            img_array = np.squeeze(cv2.imread(image_name, cv2.IMREAD_GRAYSCALE))
+        return img_array
 
 
-class ImageDataProvider(BaseDataProvider):
-    """
-    Generic data provider for images, supports gray scale and colored images.
-    Assumes that the data images and label images are stored in the same folder
-    and that the labels have a different file suffix 
-    e.g. 'train/fish_1.tif' and 'train/fish_1_mask.tif'
 
-    Usage:
-    data_provider = ImageDataProvider("..fishes/train/*.tif")
+
+class ImageDatasetProvider(BaseImageProvider):
+    def __init__(self, image_dir, c_dim=None, batch_size=None, 
+                 shuffle_data=True, buffer_size=1, repeat_data=True, a_min=None, a_max=None):
+        super(BaseImageProvider, self).__init__(image_dir, a_min, a_max)
         
-    :param search_path: a glob search pattern to find all data and label images
-    :param a_min: (optional) min value used for clipping
-    :param a_max: (optional) max value used for clipping
-    :param data_suffix: suffix pattern for the data images. Default '.tif'
-    :param mask_suffix: suffix pattern for the label images. Default '_mask.tif'
-    :param shuffle_data: if the order of the loaded file path should be randomized. Default 'True'
-    :param channels: (optional) number of channels, default=1
-    :param n_class: (optional) number of classes, default=2
-    
-    """
-    
-    def __init__(self, search_path, a_min=None, a_max=None, image_folder="images", 
-                 mask_folder=None, file_ext='*.png', shuffle_data=True, n_class = 2):
-        super(ImageDataProvider, self).__init__(a_min, a_max)
-        # self.data_suffix = data_suffix
-        # self.mask_suffix = mask_suffix
-        self.image_folder = image_folder
-        self.mask_folder = mask_folder
-        self.file_idx = -1
         self.shuffle_data = shuffle_data
-        self.n_class = n_class
+        self.repeat_data = repeat_data
+        self.buffer_size = buffer_size
 
-        self.data_images = self._find_data_files(os.path.join(search_path, image_folder, file_ext))
-        self.data_masks = self._find_data_files(os.path.join(search_path, mask_folder, file_ext))
+        # convert to constants for tf
+        self.filenames = tf.constant(self._image_list)
+        self.labels = tf.constant(_label_list)
+
+        # create dataset
+        self.data = tf.data.Dataset.from_tensor_slices((self.filenames, self.labels))
+
+        # map image in the dataset
+        self.data = self.data.map(self._load_image_func)
+
+        # process options to the dataset object
+        if batch_size is not None:
+            self.batch_size = batch_size
+        else:
+            self.batch_size = len(self._image_list) # full batch
+            print('Warning: full batch option selected, [batch_size]')
+        self.data = self.data.batch(self.batch_size)
 
         if self.shuffle_data:
-            np.random.shuffle(self.data_images)
-        
-        assert len(self.data_images) > 0, "No training files"
-        assert len(self.data_images) == len(self.data_masks), "Unequal nimages and nmasks"
-        print("Number of files used: %s" % len(self.data_images))
+            self.data = self.data.shuffle(self.buffer_size)
 
-        img = self._load_file(self.data_images[0])
-        self.channels = 1 if len(img.shape) == 2 else img.shape[-1]
-        
-    def _find_data_files(self, search_path):
-        all_files = glob.glob(search_path)
-        # return [name for name in all_files if self.data_suffix in name and not self.mask_suffix in name]
-        return [name for name in all_files]
-    
-    def _load_file(self, path, dtype=np.float32):
-        return np.array(Image.open(path), dtype)
-        # return np.squeeze(cv2.imread(image_name, cv2.IMREAD_GRAYSCALE))
+        if self.repeat_data:
+            self.data = self.data.repeat()
 
-    def _cylce_file(self):
-        self.file_idx += 1
-        if self.file_idx >= len(self.data_images):
-            self.file_idx = 0 
-            if self.shuffle_data:
-                np.random.shuffle(self.data_images)
+        # create iterator and final input tensors
+        self.iterator = self.data.make_one_shot_iterator()
+        self.image_batch, self.label_batch = iterator.get_next()
         
-    def _next_data(self):
-        self._cylce_file()
-        image_name = self.data_images[self.file_idx]
-        label_name = image_name.replace(self.image_folder, self.mask_folder)
-    
-        img = self._load_file(image_name, np.float32)
-        label = self._load_file(label_name, np.bool)
 
-        return img,label
+    def _load_image_func(filename, label):
+        """
+        load image function used to batch the files
+        """
+        image_string = tf.read_file(filename)
+        image_decoded = tf.image.decode_jpeg(image_string, c_dim=3)
+        image = tf.cast(image_decoded, tf.float32)
+        return image, label    
+
