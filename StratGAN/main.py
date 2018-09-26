f9e007cb0e361f654ca3db80778839e718666b10
amoodie <amoodie@rice.edu>
2018-09-25 22:11:55 -0500

    update to delete py files

--- StratGAN/main.py
+++ StratGAN/main.py
@@ -12,23 +12,23 @@ from model import StratGAN
 # Setup configuration
 # -----------
 config = Config()
-config.image_dir = os.path.join(os.pardir, 'data', 'multi_line')
+# config.image_dir = os.path.join(os.pardir, 'data', 'multi_line')
+config.image_dir = os.path.join(os.pardir, 'data', 'fake_single_line')
 config.image_ext = '*.png'
 config.img_verbose = True
 config.c_dim = 1
+config.override_mnist = True            # override ALL other dataset parameters and use MNIST
 
-config.batch_size = 10
+config.batch_size = 100
 config.repeat_data = True
 config.shuffle_data = True
 config.buffer_size = 10
 config.drop_remainder = True            # currently fails if false!
 
 config.z_dim = 100                      # number inputs to gener
-# config.out_h = 28
-# config.out_w = 28
 
-config.epoch = 1
-config.learning_rate = 0.0002           # optim learn rate
+config.epoch = 10
+config.learning_rate = 0.00002           # optim learn rate
 config.beta1 = 0.5                      # momentum
 
 config.log_dir = 'log'
